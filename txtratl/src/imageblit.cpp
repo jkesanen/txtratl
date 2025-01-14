965e5bd0331abcd2bf36c116761e25357cde31f4
Jani Kesänen <jani.kesanen+github@gmail.com>
2025-01-14 13:20:34 +0200

    Added also sourcePixelBytes.

--- txtratl/src/imageblit.cpp
+++ txtratl/src/imageblit.cpp
@@ -13,6 +13,7 @@ namespace txtratl
 
 void blitRGBAtoRGB_SSE3(uint8_t* dest, const uint8_t* src, const size_t width, const size_t height, const size_t destinationWidth)
 {
+    const size_t sourcePixelBytes = 4;
     const size_t destinationPixelBytes = 3;
 
     // A mask for skipping the every fourth byte out of 16 bytes.
@@ -30,7 +31,7 @@ void blitRGBAtoRGB_SSE3(uint8_t* dest, const uint8_t* src, const size_t width, c
         uint8_t* destRow = dest + row * destinationWidth * destinationPixelBytes;
 
         // Copy bytes in 16 byte (4 pixel) chunks, skip the last 4-16 bytes.
-        const uint8_t* end = src + (width - endPixels) * 4;
+        const uint8_t* end = src + (width - endPixels) * sourcePixelBytes;
         for (; src < end; src += 16, destRow += 12)
         {
             _mm_storeu_si128(reinterpret_cast<__m128i*>(destRow), _mm_shuffle_epi8(_mm_lddqu_si128(reinterpret_cast<const __m128i*>(src)), mask));
@@ -39,7 +40,7 @@ void blitRGBAtoRGB_SSE3(uint8_t* dest, const uint8_t* src, const size_t width, c
         // Copy the remaining bytes manually to avoid:
         //     1) out-of-bounds access if there's less than 16 bytes available,
         //     2) adding an extra pixel to the dest (16 byte stores and max 12 bytes of data).
-        for (size_t i = 0; i < endPixels; src += 4, destRow += destinationPixelBytes, ++i)
+        for (size_t i = 0; i < endPixels; src += sourcePixelBytes, destRow += destinationPixelBytes, ++i)
         {
             std::memcpy(destRow, src, destinationPixelBytes);
         }
@@ -48,6 +49,7 @@ void blitRGBAtoRGB_SSE3(uint8_t* dest, const uint8_t* src, const size_t width, c
 
 void blitRGBtoRGBA_SSE3(uint8_t* dest, const uint8_t* src, const size_t width, const size_t height, const size_t destinationWidth)
 {
+    const size_t sourcePixelBytes = 3;
     const size_t destinationPixelBytes = 4;
 
     // Mask for shuffling 24-bit RGB bytes into 32-bit RGBA sets.
@@ -66,7 +68,7 @@ void blitRGBtoRGBA_SSE3(uint8_t* dest, const uint8_t* src, const size_t width, c
         uint8_t* destRow = (dest + row * destinationWidth * destinationPixelBytes);
 
         // Copy bytes in 12 byte (4 pixel) chunks
-        const uint8_t* end = src + (width - endPixels) * 3;
+        const uint8_t* end = src + (width - endPixels) * sourcePixelBytes;
         for (; src < end; src += 12, destRow += 16)
         {
             __m128i bytes = _mm_shuffle_epi8(_mm_lddqu_si128(reinterpret_cast<const __m128i*>(src)), mask);
@@ -75,7 +77,7 @@ void blitRGBtoRGBA_SSE3(uint8_t* dest, const uint8_t* src, const size_t width, c
         }
 
         // Copy the remaining bytes of the row manually if there are less than 4 pixels left
-        for (size_t i = 0; i < endPixels; src += 3, destRow += destinationPixelBytes, ++i)
+        for (size_t i = 0; i < endPixels; src += sourcePixelBytes, destRow += destinationPixelBytes, ++i)
         {
             uint32_t* d = reinterpret_cast<uint32_t*>(destRow);
             // Little endian, but doesn't matter as this function is for SSE3 :)
